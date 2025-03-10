9b15d9cdc3b351c91c241968f7b13b8ed0ecf661
tinchoran <martinranzuglia@gmail.com>
2025-03-10 14:12:22 -0300

    Actualizzación de la lista de amigos con DOM

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
 // Array para almacenar los nombres 
-const amigos = [];
+let amigos = [];
 
 function agregarAmigo(){
     let amigo = document.getElementById("amigo").value;
     document.getElementById("amigo").value = "";
     if(amigo){
         amigos.push(amigo)
+        actualizarLista()
     }else{
         alert("El campo de entrada está vacío!")
     }
 }
 
 
+const lista = document.getElementById("listaAmigos");
+
+function actualizarLista(){
+    
+    lista.innerHTML = "";
+
+    for(let i = 0; i < amigos.length; i++){
+        lista.insertAdjacentHTML("beforeend", `<li>${amigos[i]}</li>`)
+    }
+}
